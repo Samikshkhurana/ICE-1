def59447a008ba8affed825020fb49e706240ecb
samikshakhurana <khurasam@sheridancollege.ca>
2023-01-23 15:20:20 -0500

    ICE version 2 test

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -4,7 +4,8 @@ package exercise1;
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
  * To be used as starting code in Exercise
- *
+ * this is branch code
+ * test 
  * @author dancye
  * @author Paul Bonenfant Jan 25, 2022 
  */
@@ -16,13 +17,15 @@ public class CardTrick {
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-		hand[i]=card;
-		System.out.println()
-            //card.setValue(insert call to random number generator here)
+		
+		
+            card.setValue((int)(1+Math.random()*13));
             // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            card.setSuit(Card.SUITS[(int)(1+Math.random()*3)]);
             // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
             //       Don't worry about duplicates at this point
+            hand[i]=card;
+		System.out.println(hand[i]);
         }
 
         // insert code to ask the user for Card value and suit, create their card
